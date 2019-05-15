eb5c2cde876590fcce4f6ae534c340f925ab9849
José Fabio Ferraz de Campos <campos_fabio@hotmail.com>
2019-05-15 11:00:06 -0300

    Navigator

--- lib/main.dart
+++ lib/main.dart
@@ -1,80 +1,60 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+    void main() {
+      runApp(MaterialApp(
+        title: 'Flutter',
+        home: FirstScreen(),
+      ));
+    }
 
-// boilerplate code
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter'),
-        ),
-        body: MyWidget(),
-      ),
-    );
-  }
-}
+    class FirstScreen extends StatelessWidget {
+      @override
+      Widget build(BuildContext context) {
+        return Scaffold(
+          appBar: AppBar(title: Text('First screen')),
+          body: Center(
+            child: RaisedButton(
+              child: Text(
+                'Go to second screen',
+                style: TextStyle(fontSize: 24),
+              ),
+              onPressed: () {
+                _navigateToSecondScreen(context);
+              },
+            )
+          ),
+        );
+      }
 
-// widget class
-class MyWidget extends StatefulWidget {
-  @override
-  _MyWidgetState createState() => _MyWidgetState();
-}
+      void _navigateToSecondScreen(BuildContext context) {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => SecondScreen(),
+            ));
+      }
+    }
 
-// state class
-// We will replace this class in each of the examples below
-class _MyWidgetState extends State<MyWidget> {
-  @override
-  Widget build(BuildContext context) {
-    return RaisedButton(
-      child: Text('Button'),
-      onPressed: () {
-        _showAlertDialog(context);
-      },
-    );
-  }
+    class SecondScreen extends StatelessWidget {
+      @override
+      Widget build(BuildContext context) {
+        return Scaffold(
+          appBar: AppBar(title: Text('Second screen')),
+          body: Center(
+            child: RaisedButton(
+              child: Text(
+                'Go back to first screen',
+                style: TextStyle(fontSize: 24),
+              ),
+              onPressed: () {
+                _goBackToFirstScreen(context);
+              },
+            ),
+          ),
+        );
+      }
 
-  void _showAlertDialog(BuildContext context) {
-    // set up the buttons
-    Widget remindButton = FlatButton(
-      child: Text("Remind me later"),
-      onPressed: () {},
-    );
-
-    Widget cancelButton = FlatButton(
-      child: Text("Cancel"),
-      onPressed: () {},
-    );
-
-    Widget launchButton = FlatButton(
-      child: Text("Launch missile"),
-      onPressed: () {
-        //onPressed: null, (Disable)
-        Navigator.of(context).pop(); // dismiss dialog
-        //launchMissile();
-      },
-    );
-    // set up the AlertDialog
-    AlertDialog alert = AlertDialog(
-      title: Text("Notice"),
-      content: Text(
-          "Launching this missile will destroy the entire universe. Is this what you intended to do?"),
-      actions: [
-        remindButton,
-        cancelButton,
-        launchButton,
-      ],
-    );
-
-    // show the dialog
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return alert;
-      },
-    );
-  }
-}
+      void _goBackToFirstScreen(BuildContext context) {
+        Navigator.pop(context);
+      }
+    }
