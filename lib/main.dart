ab98d23bf35c6096a8732b340975faa6c42099ea
José Fabio Ferraz de Campos <campos_fabio@hotmail.com>
2019-05-15 11:02:47 -0300

    Passing data

--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,64 @@
 import 'package:flutter/material.dart';
 
-    void main() {
-      runApp(MaterialApp(
-        title: 'Flutter',
-        home: FirstScreen(),
-      ));
-    }
+void main() {
+  runApp(MaterialApp(
+    title: 'Flutter',
+    home: FirstScreen(),
+  ));
+}
 
-    class FirstScreen extends StatelessWidget {
-      @override
-      Widget build(BuildContext context) {
-        return Scaffold(
-          appBar: AppBar(title: Text('First screen')),
-          body: Center(
-            child: RaisedButton(
-              child: Text(
-                'Go to second screen',
-                style: TextStyle(fontSize: 24),
+class FirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('First screen')),
+      body: Center(
+          child: RaisedButton(
+        child: Text(
+          'Go to second screen',
+          style: TextStyle(fontSize: 24),
+        ),
+        onPressed: () {
+          _navigateToSecondScreen(context);
+        },
+      )),
+    );
+  }
+
+  void _navigateToSecondScreen(BuildContext context) {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => SecondScreen(
+                text: 'Hello',
               ),
-              onPressed: () {
-                _navigateToSecondScreen(context);
-              },
-            )
+        ));
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  final String text;
+  SecondScreen({Key key, @required this.text}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Second screen')),
+      body: Center(
+        child: RaisedButton(
+          child: Text(
+            text,
+            style: TextStyle(fontSize: 24),
           ),
-        );
-      }
+          onPressed: () {
+            _goBackToFirstScreen(context);
+          },
+        ),
+      ),
+    );
+  }
 
-      void _navigateToSecondScreen(BuildContext context) {
-        Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => SecondScreen(),
-            ));
-      }
-    }
-
-    class SecondScreen extends StatelessWidget {
-      @override
-      Widget build(BuildContext context) {
-        return Scaffold(
-          appBar: AppBar(title: Text('Second screen')),
-          body: Center(
-            child: RaisedButton(
-              child: Text(
-                'Go back to first screen',
-                style: TextStyle(fontSize: 24),
-              ),
-              onPressed: () {
-                _goBackToFirstScreen(context);
-              },
-            ),
-          ),
-        );
-      }
-
-      void _goBackToFirstScreen(BuildContext context) {
-        Navigator.pop(context);
-      }
-    }
+  void _goBackToFirstScreen(BuildContext context) {
+    Navigator.pop(context);
+  }
+}
